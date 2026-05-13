7b688743d2d378000fc14f5d9c977582c02b079b
alex <alex.schmidt767@gmail.com>
2026-05-13 02:04:11 -0500

    fix: remove svg transformer that was bypassing NativeWind babel transform

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -14,8 +14,4 @@ config.resolver.nodeModulesPaths = [
   path.resolve(monorepoRoot, "node_modules"),
 ]
 
-config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer")
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg")
-config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"]
-
 module.exports = withNativeWind(config, { input: "./global.css" })
